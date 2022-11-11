116ac70a8560a585945eb73442c658b9123a13ec
YashBhardwaj2 <bhardwajyash136@gmail.com>
2022-11-11 14:21:45 +0530

    add a controller and a route

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+//all the paths that starts with home part or / basically all the url
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
