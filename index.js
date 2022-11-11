d995bcba0f0895cbd63896ef59460b5b72ddcaa6
YashBhardwaj2 <bhardwajyash136@gmail.com>
2022-11-12 00:55:46 +0530

    added partial and layout and style for layout

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 //use express router
 //all the paths that starts with home part or / basically all the url
 app.use('/',require('./routes'));
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 app.listen(port,function(err){
-    if(err){
+    if(err){ 
         console.log(`Error: ${err}`);
         return;
     }
